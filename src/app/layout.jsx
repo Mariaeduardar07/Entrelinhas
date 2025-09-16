990cf986812ea6cc04172284b55301a2c4b83f12
Maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2025-09-16 13:41:45 -0300

    feat: add header component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata = {
     title: "Entrelinhas",
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html>
-            <body>{children}</body>
+            <body>
+              <Header />
+              {children}
+              </body>
         </html>
     );
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
+          <h1 className={styles.title}>EntreLinhas</h1>
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link href="/" className={styles.navLink}>
+                Home
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/listagem" className={styles.navLink}>
+                Listagem
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/favoritos" className={styles.navLink}>
+                Favoritos
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/sobre" className={styles.navLink}>
+                Sobre Mim
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
